cf25f5ddb202ef92530974501a83791734d4a8de
pradum9112 <pradumsonkar9112@gmail.com>
2026-07-02 16:14:16 +0530

    google browser block error correction

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -187,6 +187,7 @@ const Login = () => {
         google.accounts.id.initialize({
           client_id: "556182822054-s0199us6sdlu44chlejgodafbacs3h3s.apps.googleusercontent.com",
           callback: handleCallbackResponse,
+          ux_mode: 'redirect',
         });
         google.accounts.id.renderButton(
           document.getElementById("googlebtn"),
